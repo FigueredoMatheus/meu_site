38a599e467e3a931ca86a7dc56523cdc529fd8c3
MatheusF <matheusfigueredob@gmail.com>
2022-03-10 11:58:19 -0300

    Implementa Large Skills Page

--- lib/views/skills_page.dart
+++ lib/views/skills_page.dart
@@ -15,17 +15,26 @@ class SkillsPage extends StatelessWidget {
           skillContainerWidth:
               screenWidth * 0.347 > 140 ? 140 : screenWidth * 0.347,
           titleTextFontSize:
-              screenWidth * 0.0833 > 45 ? 45 : screenWidth * 0.0833,
+              screenWidth * 0.0833 > 50 ? 50 : screenWidth * 0.0833,
         );
       } else if (screenWidth <= 950) {
-        return const SkillPageTemplate(
+        return SkillPageTemplate(
           mainContainerWidth: 470,
           skillContainerHeight: 160,
           skillContainerWidth: 140,
-          titleTextFontSize: 45,
+          titleTextFontSize:
+              screenWidth * 0.0833 > 50 ? 50 : screenWidth * 0.0833,
+        );
+      } else {
+        return SkillPageTemplate(
+          mainContainerWidth: 705,
+          skillContainerHeight: 160,
+          skillContainerWidth:
+              screenWidth * 0.147 > 150 ? 150 : screenWidth * 0.147,
+          titleTextFontSize:
+              screenWidth * 0.0833 > 50 ? 50 : screenWidth * 0.0833,
         );
       }
-      return Container(color: Colors.red);
     }
 
     return getTemplate();
