4b1cd2f5622b7e335780d2d4cfdf42e168f49b5a
MatheusF <matheusfigueredob@gmail.com>
2022-03-12 21:07:53 -0300

    Implementa lottie animation na intro

---
+++ web/lottie_scripts.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './31548-robot-says-hello.json',
+
+})
