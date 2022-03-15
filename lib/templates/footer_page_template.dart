590d9c6be4119e691b0ff61f61a5f9eceaec0167
MatheusF <matheusfigueredob@gmail.com>
2022-03-15 17:13:14 -0300

    Coloca Footer no padrão do projeto

---
+++ lib/templates/footer_page_template.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meu_site/components/social_media_icon.dart';
+import 'package:meu_site/constants/constant_spacing.dart';
+
+class FooterPageTemplate extends StatelessWidget {
+  final double screenWidth;
+  const FooterPageTemplate({
+    Key? key,
+    required this.screenWidth,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: sectionsSpace),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              SocialMediaIcon(
+                icon: FontAwesomeIcons.envelope,
+                url: 'mailto:matheusfigueredob@gmail.com',
+              ),
+              SizedBox(width: 35),
+              SocialMediaIcon(
+                icon: FontAwesomeIcons.linkedinIn,
+                url: 'https://www.linkedin.com/in/matheusfigueredo/',
+              ),
+              SizedBox(width: 35),
+              SocialMediaIcon(
+                icon: FontAwesomeIcons.github,
+                url: 'https://github.com/FigueredoMatheus?tab=repositories',
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: screenWidth * 0.83,
+            child: Text(
+              'copyright © 2022 Matheus Figueredo. Todos os direitos reservados.',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.sanchez(
+                fontSize: 14,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
